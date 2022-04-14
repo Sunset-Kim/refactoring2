1bc0b88311fae44cf4141555237a15fcca9bb4d5
Sunset-kim <wood2002@naver.com>
2022-04-14 12:51:38 +0900

    1.4 함수쪼개기 - switch 문을 따로 분리

--- 1_start/1.ts
+++ 1_start/1.ts
@@ -23,26 +23,7 @@ function statement(invoice: Invoice, plays: PlayList) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playId];
-    let thisAmount = 0;
-    switch (play.type) {
-      case "tragedy": // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-
-      case "comedy": // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 :${play.type}`);
-    }
+    let thisAmount = amountFor(play, perf);
 
     // 포인트를 적립한다.
     volumCredits += Math.max(perf.audience - 30, 0);
@@ -63,9 +44,29 @@ function statement(invoice: Invoice, plays: PlayList) {
   return result;
 }
 
-// play, thisAmount, perf : 변수를 의존하고 있음
-// thisAmount
+// 함수추출하기 - thisAmount, play, perf 의존 변수를 인자로 받거나 맥락상으로 안으로 위치 시킴
+function amountFor(play: Play, perf: Performances) {
+  let thisAmount = 0;
 
-// function amountFor(play: Play, perf: Performances) {
-//   let thisAmount = 0;
-// }
+  switch (play.type) {
+    case "tragedy": // 비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+
+    case "comedy": // 희극
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르 :${play.type}`);
+  }
+
+  return thisAmount;
+}
